bd84fa127df8f78c18794d3e4cb14f544864ea29
adankevych <adankevych@kse.org.ua>
2024-12-16 22:07:19 +0200

    "Favourite" page

--- Reading_Tracker/Sources/Controllers/FavouriteViewController.swift
+++ Reading_Tracker/Sources/Controllers/FavouriteViewController.swift
@@ -7,12 +7,77 @@
 
 import UIKit
 import SwiftUI
+import SnapKit
 
 class FavouriteViewController: UIViewController {
+
+    private var booksCollectionView: UICollectionView!
+    private var quotesCollectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupBackground()
+
+        let favoriteBooksLabel = UILabel()
+        favoriteBooksLabel.text = "Favourite Books"
+        favoriteBooksLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        favoriteBooksLabel.textAlignment = .left
+        favoriteBooksLabel.textColor = .black
+
+        booksCollectionView = createCollectionView()
+
+        let savedQuotesLabel = UILabel()
+        savedQuotesLabel.text = "Saved Quotes"
+        savedQuotesLabel.font = UIFont.boldSystemFont(ofSize: 24)
+        savedQuotesLabel.textAlignment = .left
+        savedQuotesLabel.textColor = .black
+
+        quotesCollectionView = createCollectionView()
+
+        let stackView = UIStackView(arrangedSubviews:
+                [favoriteBooksLabel,
+                 booksCollectionView,
+                 savedQuotesLabel,
+                 quotesCollectionView]
+        )
+
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.alignment = .fill
+
+        view.addSubview(stackView)
+
+        stackView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            $0.leading.equalTo(view).offset(16)
+            $0.trailing.equalTo(view).offset(-16)
+            $0.bottom.lessThanOrEqualTo(view).offset(-20)
+        }
+
+        booksCollectionView.snp.makeConstraints { $0.height.equalTo(250) }
+
+        quotesCollectionView.snp.makeConstraints { $0.height.equalTo(150) }
+    }
+
+    private func createCollectionView() -> UICollectionView {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 150, height: 200)
+        layout.minimumLineSpacing = 16
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.showsHorizontalScrollIndicator = false
+
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+
+        return collectionView
+    }
+
+    private func setupBackground() {
         let gradientLayer = CAGradientLayer()
 
         gradientLayer.frame = view.bounds
@@ -27,4 +92,105 @@ class FavouriteViewController: UIViewController {
 
         view.layer.insertSublayer(gradientLayer, at: 0)
     }
+
+}
+
+extension FavouriteViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == booksCollectionView {
+            return ProcessingBookJSON.shared.favouriteBooks().count
+        } else {
+            return ProcessingQuoteJSON.shared.amountOfQuotes()
+        }
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == booksCollectionView {
+            return configureBookCell(for: collectionView, at: indexPath)
+        } else {
+            return configureQuoteCell(for: collectionView, at: indexPath)
+        }
+    }
+
+    private func configureBookCell(for collectionView: UICollectionView, at indexPath:
+                        IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.layer.cornerRadius = 8
+
+        let book = ProcessingBookJSON.shared.favouriteBooks()[indexPath.item]
+
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 8
+        imageView.clipsToBounds = true
+        imageView.image = UIImage(named: book.img)
+        cell.contentView.addSubview(imageView)
+
+        imageView.snp.makeConstraints {
+            $0.top.equalTo(cell.contentView).offset(5)
+            $0.left.equalTo(cell.contentView).offset(5)
+            $0.right.equalTo(cell.contentView).offset(-5)
+            $0.height.equalTo(cell.bounds.height * 0.75)
+        }
+
+        let label = UILabel()
+        label.text = book.title
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.numberOfLines = 0
+        label.textColor = .black
+        cell.contentView.addSubview(label)
+
+        label.snp.makeConstraints {
+            $0.top.equalTo(imageView.snp.bottom).offset(5)
+            $0.left.equalTo(cell.contentView).offset(10)
+            $0.right.equalTo(cell.contentView).offset(-10)
+            $0.height.equalTo(cell.bounds.height * 0.3)
+        }
+
+        return cell
+    }
+
+    private func configureQuoteCell(for collectionView: UICollectionView, at indexPath:
+                        IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.layer.cornerRadius = 8
+        cell.backgroundColor = .lightGreen
+
+        let quote = ProcessingQuoteJSON.shared.quotes[indexPath.item]
+
+        let label = UILabel()
+        label.text = "“\(quote.quote)“"
+        label.textAlignment = .center
+        label.font = UIFont.italicSystemFont(ofSize: 14)
+        label.numberOfLines = 0
+        label.textColor = .black
+        cell.contentView.addSubview(label)
+
+        label.snp.makeConstraints {
+            $0.top.equalTo(cell.contentView).offset(10)
+            $0.left.equalTo(cell.contentView).offset(10)
+            $0.right.equalTo(cell.contentView).offset(-10)
+            $0.height.equalTo(cell.bounds.height * 0.6)
+        }
+
+        let labelTitle = UILabel()
+        labelTitle.text = "from: “\(quote.title)“"
+        labelTitle.textAlignment = .center
+        labelTitle.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        labelTitle.textColor = .gray
+        cell.contentView.addSubview(labelTitle)
+
+        labelTitle.snp.makeConstraints {
+            $0.top.equalTo(label.snp.bottom).offset(5)
+            $0.left.equalTo(cell.contentView).offset(10)
+            $0.right.equalTo(cell.contentView).offset(-10)
+            $0.height.equalTo(20)
+        }
+
+        return cell
+    }
+
 }
