e2fbbefb024bd1eaf0aaef10af72439f373951b3
adankevych <adankevych@kse.org.ua>
2024-12-16 15:35:10 +0200

    Background gradient

--- Reading_Tracker/Sources/Controllers/FavouriteViewController.swift
+++ Reading_Tracker/Sources/Controllers/FavouriteViewController.swift
@@ -9,5 +9,22 @@ import UIKit
 import SwiftUI
 
 class FavouriteViewController: UIViewController {
-    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let gradientLayer = CAGradientLayer()
+
+        gradientLayer.frame = view.bounds
+
+        gradientLayer.colors = [
+            UIColor(red: 0.66, green: 0.88, blue: 0.44, alpha: 1.0).cgColor,
+            UIColor(red: 0.22, green: 0.44, blue: 0.11, alpha: 1.0).cgColor
+        ]
+
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
