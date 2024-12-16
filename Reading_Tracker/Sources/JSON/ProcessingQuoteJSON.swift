f8b426db191c364a9575b05e97725fc1cf118f26
adankevych <adankevych@kse.org.ua>
2024-12-16 13:46:56 +0200

    Processing quotes

---
+++ Reading_Tracker/Sources/JSON/ProcessingQuoteJSON.swift
@@ -0,0 +1,40 @@
+//
+//  ProcessingQuoteJSON.swift
+//  Reading_Tracker
+//
+//  Created by Данькевич Анастасія on 16.12.2024.
+//
+
+import Foundation
+import UIKit
+
+final class ProcessingQuoteJSON {
+    
+    let quotesPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("Quotes.json")
+
+    var quotes: [Quote] = []
+
+    func parseQuotes() throws -> [Quote] {
+        let contents = try Data(contentsOf: quotesPath)
+        let quotes = try JSONDecoder().decode([Quote].self, from: contents)
+
+        return quotes
+    }
+
+    func writeQuotes() throws {
+        let contents = try JSONEncoder().encode(self.quotes)
+        try contents.write(to: quotesPath)
+//        print("Path \(quotesPath)")
+    }
+
+    func addQuote(quote: Quote) throws {
+        let quotes = try parseQuotes()
+        self.quotes = quotes
+        self.quotes.append(quote)
+        try writeQuotes()
+    }
+
+    func amountOfQuotes() -> Int {
+        return self.quotes.count
+    }
+}
